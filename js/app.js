f6af6a318a3b1b19df3998f505bc71a49de7421f
Badal Moradia <badal@i360llc.com>
2018-06-21 18:00:42 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- js/app.js
+++ js/app.js
@@ -8,9 +8,34 @@ const $body = $('body');
 const $container = $('#container');
 //console.log($container);
 
-
+//First Year
 const $h1 = $('<h1>');
 //console.log($h1)
 
 $h1.text('First Year at Hogwarts');
 $body.append($h1);
+
+
+
+//Second Year
+const $h2 = $('<h2>');
+
+$h2.text('Fred Flubberton');
+$body.append($h2);
+
+const $h3 = $('<h3>');
+
+$h3.text('Ravenclaw');
+$body.append($h3);
+
+const $h4 = $('<h4>');
+
+$h4.text('Binkers');
+$body.append($h4);
+$h4.addClass('Owl');
+
+const $h4_2 = $('<h4>');
+
+$h4_2.text('13" Kingwood wand with an alligator scale core');
+$body.append($h4_2);
+
